ea6fda2e194d92c10fdb1c2b264fcf11a8d2508a
Ambre Lis <alis@cfai-formation.fr>
2026-01-04 17:39:03 +0100

    fix: fonctionnement de l'api a l etat initial

---
+++ api/config.js
@@ -0,0 +1,10 @@
+module.exports =  {
+    ACCESS_TOKEN_SECRET : "EMMA123",
+    BDD : {
+    "host" :"dpg-d41mvac9c44c73d001ag-a.oregon-postgres.render.com",
+    "port" : "5432",
+    "user" : "pollution_o50q_user",
+    "password" : "yqQJvxG6udcOC4XXXAVEBBjv8ywSOxC4",
+    "bdname" :"pollution_o50q" 
+    }
+}
